c92382f55bf0757fe69ab1b3ca9b77df5135a523
Jenah <contaprafazerqualquercoisa4151@gmail.com>
2024-12-31 15:45:52 -0300

    incrementado reação do site caso o usuario não preencha o formuario

--- script.js
+++ script.js
@@ -12,19 +12,25 @@ formulario.addEventListener('submit', function(e) {
     let Bnumber = document.getElementById('numero-B')
     let containerMessage = document.getElementById('conclusion-message')
 
-    if (reportaBurro(Anumber.value, Bnumber.value)) {
-        containerMessage.innerHTML = 'Parabéns, você definitivamente sabe o mínimo do mínimo de matemática.'
-        containerMessage.style.display = 'block'
-        containerMessage.style.backgroundColor = 'green'
-
-        Anumber.value = ''
-        Bnumber.value = ''
+    if (Anumber.value || Bnumber.value){
+        if (reportaBurro(Anumber.value, Bnumber.value)) {
+            containerMessage.innerHTML = 'Parabéns, você definitivamente sabe o mínimo do mínimo de matemática.'
+            containerMessage.style.display = 'block'
+            containerMessage.style.backgroundColor = 'green'
+            
+            Anumber.value = ''
+            Bnumber.value = ''
+        } else {
+            containerMessage.innerHTML = 'Jumento da peste, vai aprender a contar antes de vir me usar.'
+            containerMessage.style.display = 'block'
+            containerMessage.style.backgroundColor = 'red'
+            
+            Anumber.value = ''
+            Bnumber.value = ''
+        }
     } else {
-        containerMessage.innerHTML = 'Jumento da peste, vai aprender a contar antes de vir me usar.'
+        containerMessage.innerHTML = 'Filho, c tem que por algum numero dentro dessas duas caixinhas aqui tá'
         containerMessage.style.display = 'block'
-        containerMessage.style.backgroundColor = 'red'
-
-        Anumber.value = ''
-        Bnumber.value = ''
+        containerMessage.style.backgroundColor = 'fuchsia'
     }
 })
